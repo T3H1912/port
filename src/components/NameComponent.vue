299f392f5327684e03cad76eb34fd226c14f82bb
T3H1912 <76159846+T3H1912@users.noreply.github.com>
2021-03-12 14:19:54 +0100

    Update NameComponent.vue

--- src/components/NameComponent.vue
+++ src/components/NameComponent.vue
@@ -148,7 +148,7 @@ export default {
 
         onClick() {
               axios({
-                    url: 'https://dl.dropbox.com/s/bleb2umdd0mrqij/Tomas_Komrska_CV_.pdf?dl=1',
+                    url: 'https://dl.dropbox.com/s/hg9hlk08wyjrd4c/Tomas_Komrska_CV.pdf?dl=1', 
                     method: 'GET',
                     responseType: 'blob',
                 }).then((response) => {
